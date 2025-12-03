d7afa4d8d0e63a81f59d71a84b2207acbb78753a
Johnny Gibson <jgsteeler@gmail.com>
2025-12-02 22:30:25 -0600

    refactor: remove duplicate import statements in UserController tests

--- packages/div-flo-api/tests/controllers/UserController.test.ts
+++ packages/div-flo-api/tests/controllers/UserController.test.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
 import { describe, it, expect, beforeEach, vi } from 'vitest';
-import 'reflect-metadata';
-import { describe, it, expect, beforeEach, vi } from 'vitest';
 import { UserController } from '../../src/controllers/UserController';
 import { IUserService } from '@div-flo/models';
 import { Request, Response } from 'express';
